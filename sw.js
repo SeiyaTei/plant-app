1e84ec7130bb173abc5855e57fd1ffaa6eeede80
SeiyaTei <seiyatei@outlook.jp>
2026-08-14 16:54:32 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,33 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('plant-app-v1').then((cache) => {
-      return cache.addAll(['./', './index.html']);
+// バージョン番号を 'v2' にすることで、スマホに強制更新を促します
+const CACHE_NAME = 'plant-app-v2'; 
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key);
+        }
+      }));
     })
   );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
     })
   );
 });
